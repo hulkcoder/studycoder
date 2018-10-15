c06e69552034dcea6345316f2cc24d9026a1d4d3
Steve Tan Chin Leong <39689847+hulkcoder@users.noreply.github.com>
2018-10-15 16:05:36 +0800

    Update getxml.ps1

--- powershell/getxml.ps1
+++ powershell/getxml.ps1
@@ -1,2 +1,18 @@
 $atsfile = [xml](Get-Content "content.xml")
 $atsfile.ApplicationConfig.key | Where-Object {$_.name -eq "abc"}
+
+$servers = @("aserver", "bserver", "cserver","dserver","eserver")
+$servers |foreach {	
+	Write-Host "Hostname:$_"
+	$dmsfile = [xml](Get-Content "\\$_\x$\config\web\ms.xml")
+	$o = $dmsfile.ApplicationConfig.key | Where-Object {$_.name -eq "abc"}	 
+	[string]$os = $o.value
+	if ($os -ne "") {
+		Write-Host "abc:$os"
+	}
+	$o = $dmsfile.ApplicationConfig.key | Where-Object {$_.name -eq "def"}
+	[string]$os = $o.value
+	if ($os -ne "") {
+		Write-Host "def:$os"
+	}
+}
